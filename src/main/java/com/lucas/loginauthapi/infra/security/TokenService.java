ba6d2b2b09fd7cda781c1297c6c00c0fb850df98
Lucas Muniz <lucaslima1048@gmail.com>
2024-04-25 00:17:39 -0300

    update token service

--- src/main/java/com/lucas/loginauthapi/infra/security/TokenService.java
+++ src/main/java/com/lucas/loginauthapi/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.lucas.loginauthapi.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.lucas.loginauthapi.domain.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -32,6 +33,20 @@ public class TokenService {
        }catch (JWTCreationException exception){
            throw new RuntimeException("Error while authenticating");
        }
+    }
+
+    public String validateToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("login-auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception){
+            return null;
+        }
     }
 
     private Instant generateExpirationDate(){
