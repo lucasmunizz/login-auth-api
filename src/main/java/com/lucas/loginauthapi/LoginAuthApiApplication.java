bf0ccf32737da748fe447b6723b9fac56276d9cc
Lucas Muniz <lucaslima1048@gmail.com>
2024-04-25 00:30:46 -0300

    create securityFilter

--- src/main/java/com/lucas/loginauthapi/LoginAuthApiApplication.java
+++ src/main/java/com/lucas/loginauthapi/LoginAuthApiApplication.java
@@ -1,7 +1,10 @@
 package com.lucas.loginauthapi;
 
+import com.lucas.loginauthapi.domain.user.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class LoginAuthApiApplication {
@@ -9,5 +12,4 @@ public class LoginAuthApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LoginAuthApiApplication.class, args);
 	}
-
 }
