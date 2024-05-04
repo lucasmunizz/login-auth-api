2a072a1dee1c0892d1053e81c9bd250e188d4e7d
Lucas Muniz <lucaslima1048@gmail.com>
2024-05-04 09:37:46 -0300

    editing auth controller

--- src/main/java/com/lucas/loginauthapi/controllers/AuthController.java
+++ src/main/java/com/lucas/loginauthapi/controllers/AuthController.java
@@ -3,6 +3,7 @@ package com.lucas.loginauthapi.controllers;
 
 import com.lucas.loginauthapi.domain.user.User;
 import com.lucas.loginauthapi.dto.LoginRequestDTO;
+import com.lucas.loginauthapi.dto.RegisterRequestDTO;
 import com.lucas.loginauthapi.dto.ResponseDTO;
 import com.lucas.loginauthapi.infra.security.TokenService;
 import com.lucas.loginauthapi.repositories.UserRepository;
@@ -11,8 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RequestMapping("/auth")
 @RestController
@@ -25,11 +27,25 @@ public class AuthController {
 
     public ResponseEntity<ResponseDTO> login(@RequestBody LoginRequestDTO body){
         User user = this.repository.findByEmail(body.email()).orElseThrow(() -> new RuntimeException("User not found"));
-        if (encoder.matches(user.getPassword(), body.password())){
+        if (encoder.matches(body.password(), user.getPassword())){
             String token = this.tokenService.generateToken(user);
             return ResponseEntity.ok(new ResponseDTO(user.getName(), token));
         }
+        return ResponseEntity.badRequest().build();
+    }
 
+    public ResponseEntity<ResponseDTO> register(@RequestBody RegisterRequestDTO body){
+        Optional<User> user = this.repository.findByEmail(body.email());
+
+        if (user.isEmpty()){
+            User newUser = new User();
+            newUser.setEmail(body.email());
+            newUser.setName(body.name());
+            newUser.setPassword(encoder.encode(body.password()));
+            repository.save(newUser);
+            String token = this.tokenService.generateToken(newUser);
+            return ResponseEntity.ok(new ResponseDTO(newUser.getName(), token));
+        }
         return ResponseEntity.badRequest().build();
     }
 
--- src/main/java/com/lucas/loginauthapi/domain/user/User.java
+++ src/main/java/com/lucas/loginauthapi/domain/user/User.java
@@ -3,6 +3,7 @@ package com.lucas.loginauthapi.domain.user;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
@@ -10,6 +11,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     @Id
